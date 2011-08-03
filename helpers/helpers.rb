1ef752a0a490b383d70fb89b515686a4d205dd9e
Tomas Valent (eq8) <tomas.valent@gmail.com>
2011-08-03 16:23:06 +0100

    Just simple content

---
+++ helpers/helpers.rb
@@ -0,0 +1,55 @@
+
+
+helpers do
+  def link_to(url, text=url, opts={}) 
+    attributes = ""
+    opts.each { |key,value| attributes << key.to_s << "=\"" << value << "\" "}
+    "<a href=\"#{url}\" #{attributes}>#{text}</a>"
+  end
+
+  def convert_to_link_name(file_name)
+    file_name.gsub("_", ' ')
+  end
+
+  def convert_to_link_path_name(file_name)
+    file_name
+  end
+
+  def link_to_file(what, file_name)
+    path_to_file = settings.reder_w_files_patern.gsub(':what', what.to_s).gsub(':name', file_name.to_s)
+    link_to path_to_file, convert_to_link_name(file_name)
+  end
+
+  def coderay(text,lang='ruby')
+    CodeRay.scan(text,lang).div(:css => :class)
+  end
+
+
+def markdown(text)
+  options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode]
+ # options = [:filter_html, :hard_wrap, :autolink, :no_intraemphasis]
+  syntax_highlighter(Redcarpet.new(text, *options).to_html)
+end
+
+def syntax_highlighter(html)
+  doc = Nokogiri::HTML(html)
+  doc.search("//pre[@lang]").each do |pre|
+    pre.replace coderay(pre.text.rstrip, pre[:lang])
+  end
+  doc.to_s
+end
+
+
+  def read_w_file(file_name)
+    file = File.open( w_path(file_name) , "r")
+    file_content = file.read
+    file.close
+
+    file_content
+  end
+
+  def dir_w_listing(folder_name)
+      Dir.entries( w_path(folder_name )) - ['.', '..']
+  end
+
+end
--- scrapbook.rb
+++ scrapbook.rb
@@ -7,68 +7,17 @@ require 'redcarpet'
 require 'coderay'
 require 'nokogiri'
 
+require "#{File.dirname(__FILE__)}/helpers/helpers.rb"
+
 
 set :owner_name => "eq8"
 set :reder_w_files_patern => "/#{settings.owner_name}s_:what/on_:name" #if you are changing this line, be sure you change the patern in w_path helper
 set :w_files_path => "public/w/"
 
 helpers do
-
   def w_path(file_string=nil) 
     file_string.nil? ? "#{settings.w_files_path}#{params[:what]}s/#{params[:name]}" : "#{settings.w_files_path}#{file_string}"
   end
-
-  def link_to(url, text=url, opts={}) 
-    attributes = ""
-    opts.each { |key,value| attributes << key.to_s << "=\"" << value << "\" "}
-    "<a href=\"#{url}\" #{attributes}>#{text}</a>"
-  end
-
-  def convert_to_link_name(file_name)
-    file_name.gsub("_", ' ')
-  end
-
-  def convert_to_link_path_name(file_name)
-    file_name
-  end
-
-  def link_to_file(what, file_name)
-    path_to_file = settings.reder_w_files_patern.gsub(':what', what.to_s).gsub(':name', file_name.to_s)
-    link_to path_to_file, convert_to_link_name(file_name)
-  end
-
-  def coderay(text,lang='ruby')
-    CodeRay.scan(text,lang).div(:css => :class)
-  end
-
-
-def markdown(text)
-  options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode]
- # options = [:filter_html, :hard_wrap, :autolink, :no_intraemphasis]
-  syntax_highlighter(Redcarpet.new(text, *options).to_html)
-end
-
-def syntax_highlighter(html)
-  doc = Nokogiri::HTML(html)
-  doc.search("//pre[@lang]").each do |pre|
-    pre.replace coderay(pre.text.rstrip, pre[:lang])
-  end
-  doc.to_s
-end
-
-
-  def read_w_file(file_name)
-    file = File.open( w_path(file_name) , "r")
-    file_content = file.read
-    file.close
-
-    file_content
-  end
-
-  def dir_w_listing(folder_name)
-      Dir.entries( w_path(folder_name )) - ['.', '..']
-  end
-
 end
 
 #ultra simple & dum I18n
@@ -80,12 +29,8 @@ protected
   def self.translation
     {
       :links => {
-        :quick_notes => "Quick Notes",
-        :scraps => "Scraps",
-        :articles => "Articles",
         :home => "Home",
         :links => "Links"
-
       }
     }
   end
