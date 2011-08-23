6943cc56331f61c8238f3d72e5473146857a12b1
Tomas Valent (eq8) <tomas.valent@gmail.com>
2011-08-23 17:03:57 +0100

    more wisdom

--- plugins/routings/readed_articles.rb
+++ plugins/routings/readed_articles.rb
@@ -1,5 +1,5 @@
 get '/archive_of_articles' do
-      @title = "Archive of articles/posts I have red"
+      @title = "Archive of articles I had read"
       @file_content = read_w_file('other/readed_articles') 
       haml :markdown_read
 end
