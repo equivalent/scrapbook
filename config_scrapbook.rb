c810598b104bbfb19e20efa7a4d6e87a0b5ca25b
Tomas Valent (eq8) <tomas.valent@gmail.com>
2011-09-02 16:38:09 +0100

    Just simple content

--- config_scrapbook.rb
+++ config_scrapbook.rb
@@ -1,5 +1,5 @@
-set :owner_name => "eq8"
-set :reder_w_files_patern => "/#{settings.owner_name}s_:what/on_:name" #if you are changing this line, be sure you change the patern in helper/w_path.rb helper
+set :owner_name => "Equivalent"
+set :reder_w_files_patern => "/#{settings.owner_name.downcase}s_:what/on_:name" #if you are changing this line, be sure you change the patern in helper/w_path.rb helper
 set :w_files_path => "wisdom_inside/"
 
 #if you want restrict access
--- scrapbook.rb
+++ scrapbook.rb
@@ -51,14 +51,14 @@ end
 
 
 get settings.reder_w_files_patern do
-  @title= "#{settings.owner_name} #{params[:what]} on #{convert_to_link_name(params[:name])} "
+  @title= "#{settings.owner_name}'s #{params[:what]} on #{convert_to_link_name(params[:name])} "
   @h1_name = convert_to_link_name(params[:name]).capitalize
   @h1_what = params[:what]
   begin
       @file_content = read_w_file(nil) 
       haml params['what'].to_sym
   rescue Errno::ENOENT
-      haml :not_found
+     redirect '/'
   end
 end
 
