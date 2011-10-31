1feeccf9532924c5feb2e9febbefbb972010c8b5
Tomas Valent (eq8) <tomas.valent@gmail.com>
2011-10-31 13:46:15 +0000

    new plugin => readed articles on webdesign

--- plugins/routings/readed_articles.rb
+++ plugins/routings/readed_articles.rb
@@ -1,5 +1,11 @@
-get '/archive_of_articles' do
+get '/archive_of_developer_articles' do
       @title = "Archive of articles I had read"
       @file_content = read_w_file('other/readed_articles') 
+      haml :markdown_read
+end
+
+get '/archive_of_designer_articles' do
+      @title = "Archive of articles I had read"
+      @file_content = read_w_file('other/readed_articles_html') 
       haml :markdown_read
 end
